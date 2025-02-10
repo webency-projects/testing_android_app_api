94f9b2c4ec34b4e9e47655420d448fff109af23f
webency <webency@mail.ru>
2025-02-10 10:24:38 +0300

    change reoutes

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,18 +1,25 @@
 from fastapi.routing import APIRouter
-from .schemas import SmsReceiver
+from .schemas import SmsRequest, PushRequest, DeviceInfoRequest
 
 router = APIRouter()
 
 
 @router.post("/sms")
-async def sms_receiver(sms: SmsReceiver):
+async def sms_receiver(sms: SmsRequest):
     print("-----------------")
     print(sms)
     print("-----------------")
 
 
 @router.post("/push")
-async def sms_receiver(sms: SmsReceiver):
+async def push_receiver(push: PushRequest):
     print("-----------------")
-    print(sms)
+    print(push)
+    print("-----------------")
+
+
+@router.post("/device")
+async def device_info(device: DeviceInfoRequest):
+    print("-----------------")
+    print(device)
     print("-----------------")
--- app/api/schemas.py
+++ app/api/schemas.py
@@ -1,6 +1,24 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
-class SmsReceiver(BaseModel):
-    address: str
+class SmsRequest(BaseModel):
+    sender: str
     message: str
+    timestamp: int
+
+
+class PushRequest(BaseModel):
+    sender: str
+    message: str
+    timestamp: int
+
+
+class DeviceInfoRequest(BaseModel):
+    deviceId: Optional[str] = None
+    device: Optional[str] = None
+    manufacturer: Optional[str] = None
+    model: Optional[str] = None
+    brand: Optional[str] = None
+    versionRelease: Optional[str] = None
+    sdkInt: Optional[int] = None
